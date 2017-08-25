79ec721339b8f3c32b401e2955000725df97f84f
Curtis Ekstrom <code@fauxton.io>
2017-08-25 20:43:24 +0200

    Add context to test helper
    This will not be normally needed, but could be helpful in cases where the child is more generic and is used in multiple contexts on a page

--- test-support/helpers/find-wrapper.js
+++ test-support/helpers/find-wrapper.js
@@ -1,12 +1,21 @@
 import Ember from 'ember';
 
-const { isNone } = Ember;
+export function convertSelector(selector) {
+  if (/^[\w-]+$/.test(selector)) {
+    selector = `[data-test-${selector}]`;
+  }
+  return selector;
+}
+
 
 export default function () {
   Ember.Test.unregisterHelper('find');
-  Ember.Test.registerHelper('find', (app, selector) => {
-    if (/^[\w-]+$/.test(selector)) {
-      selector = `[data-test-${selector}]`;
+  Ember.Test.registerHelper('find', (app, selector, context) => {
+    selector = convertSelector(selector);
+
+    if (context) {
+      context = convertSelector(context);
+      return Ember.$(selector, context);
     }
 
     return Ember.$(selector);
