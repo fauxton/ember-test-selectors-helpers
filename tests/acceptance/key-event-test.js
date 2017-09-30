27060fda38cafade46e3b25982cd67c95377d412
Curtis Ekstrom <code@fauxton.io>
2017-09-30 03:22:48 +0200

    Improve test phrasing

--- tests/acceptance/key-event-test.js
+++ tests/acceptance/key-event-test.js
@@ -3,7 +3,7 @@ import moduleForAcceptance from '../../tests/helpers/module-for-acceptance';
 
 moduleForAcceptance('Acceptance | key event');
 
-test('visiting /key-event', async function(assert) {
+test('keyEvent works as expected', async function(assert) {
   await visit('/key-event');
 
   await keyEvent('press-enter-key', 'keypress', 13);
