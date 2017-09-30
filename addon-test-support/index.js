83d08be245fa54a0c36c84429e7c8e3e2aa0d0aa
Curtis Ekstrom <code@fauxton.io>
2017-09-30 13:53:39 +0200

    Re-export remaining helpers and add to integration test

--- addon-test-support/index.js
+++ addon-test-support/index.js
@@ -1,1 +1,5 @@
 export { find } from './find';
+export { fillIn } from './fill-in';
+export { click } from './click';
+export { keyEvent } from './key-event';
+export { triggerEvent } from './trigger-event';
--- tests/integration/components/test-all-helpers-test.js
+++ tests/integration/components/test-all-helpers-test.js
@@ -1,6 +1,6 @@
 import { moduleForComponent, test } from 'ember-qunit';
 import hbs from 'htmlbars-inline-precompile';
-import { find } from 'ember-test-selectors-helpers';
+import { find, fillIn, click, keyEvent, triggerEvent } from 'ember-test-selectors-helpers';
 
 moduleForComponent('test-all-helpers', 'Integration | Component | test all helpers', {
   integration: true
@@ -9,5 +9,15 @@ moduleForComponent('test-all-helpers', 'Integration | Component | test all helpe
 test('can use helpers in integration tests', async function(assert) {
   await this.render(hbs`{{test-all-helpers}}`);
 
-  assert.ok(find('foo-bar'), 'find helper works');
+  assert.ok(find('foo-bar').length, 'find helper works');
+
+  await fillIn('some-input', 'some text');
+
+  assert.equal(find('some-input').val(), 'some text');
+
+  await click('a-button');
+  await keyEvent('key-event-target', 'keypress', 13);
+  await triggerEvent('blur-target', 'blur');
+
+  assert.ok(find('blur-target').hasClass('blurred'));
 });
